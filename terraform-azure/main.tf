9223aa541acb3a42e942f13bae4401930e1401bf
dmonakh <d.monakh@it-sprout.org>
2023-04-16 14:14:00 +0300

    up main

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -90,16 +90,16 @@ resource "azurerm_mssql_server" "product" {
   administrator_login          = "mysqladminun"
   administrator_login_password = "H@Sh1CoR3!"
 
-  sku_name   = "B_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
+  # sku_name   = "B_Gen5_2"
+  # storage_mb = 5120
+  # version    = "5.7"
 
-  auto_grow_enabled                 = true
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = false
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  # auto_grow_enabled                 = true
+  # geo_redundant_backup_enabled      = false
+  # infrastructure_encryption_enabled = false
+  # public_network_access_enabled     = false
+  # ssl_enforcement_enabled           = true
+  # ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
 # Create BD
