b82a55e389e024d4487125f02494bddefb899e32
dmonakh <d.monakh@it-sprout.org>
2023-04-16 13:54:16 +0300

    test v2 create BD

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -107,7 +107,7 @@ resource "azurerm_sql_database" "product" {
   name                = "my-bd-for-wp"
   resource_group_name = azurerm_resource_group.product.name
   location            = azurerm_resource_group.product.location
-  server_name         = azurerm_sql_server.product.name
+  server_name         = azurerm_mysql_server.product.name
   requested_service_objective_name = "S0"
 
   tags = {
