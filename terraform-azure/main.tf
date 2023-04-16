f72a2a04b756283fd03a0be92047a8c43a388650
dmonakh <d.monakh@it-sprout.org>
2023-04-16 13:25:20 +0300

    destroy

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -97,7 +97,7 @@ resource "azurerm_mysql_server" "product" {
   auto_grow_enabled                 = true
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
-  public_network_access_enabled     = true
+  public_network_access_enabled     = false
   ssl_enforcement_enabled           = true
   ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
