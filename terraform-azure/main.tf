c37d0f361c1216479171dfe4b6b1d81aaf6ffc94
dmonakh <d.monakh@it-sprout.org>
2023-04-17 17:55:10 +0300

    destroy

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -101,9 +101,14 @@ resource "azurerm_mysql_server" "product" {
   public_network_access_enabled     = false
   ssl_enforcement_enabled           = false
   ssl_minimal_tls_version_enforced  = "TLS1_2"
-  firewall_rule {
-    name                     = "AllowAll"
-    start_ip_address         = "0.0.0.0"
-    end_ip_address           = "255.255.255.255"
-  }
+}
+
+#Create firewall rule
+resource "azurerm_mysql_firewall_rule" "allow_all" {
+  name                = "AllowAll"
+  resource_group_name = azurerm_resource_group.product.name
+  server_name         = azurerm_mysql_server.product.name
+
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "255.255.255.255"
 }
