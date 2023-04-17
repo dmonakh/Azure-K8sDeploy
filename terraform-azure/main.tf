e370b54f00e0d2e49ef1a64ccf70f4b756d3d779
dmonakh <d.monakh@it-sprout.org>
2023-04-17 22:06:02 +0300

    test main

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -99,6 +99,4 @@ resource "azurerm_mysql_server" "product" {
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_0"
 }
