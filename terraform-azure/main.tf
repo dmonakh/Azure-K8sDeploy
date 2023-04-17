449d28c291cc9953595a02ccd8f6309aec1f9f56
dmonakh <d.monakh@it-sprout.org>
2023-04-17 18:17:56 +0300

    fix main

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -99,8 +99,8 @@ resource "azurerm_mysql_server" "product" {
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = false
-  # ssl_enforcement_enabled           = false
-  # ssl_minimal_tls_version_enforced  = "TLS1_2"
+  ssl_enforcement_enabled           = false
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
 #Create firewall rule
