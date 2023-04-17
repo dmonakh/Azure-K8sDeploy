3458be4097c75169aa8c00272238f372d98cc412
dmonakh <d.monakh@it-sprout.org>
2023-04-17 18:22:19 +0300

    fix tls in main

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -100,7 +100,7 @@ resource "azurerm_mysql_server" "product" {
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = false
   ssl_enforcement_enabled           = false
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  ssl_minimal_tls_version_enforced  = "TLS1_0"
 }
 
 #Create firewall rule
